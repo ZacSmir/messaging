16143f14399ac13308aa4f6b05e1427464855a78
ZacSmir <Zach@smirin.us>
2016-09-30 08:40:20 -0700

    Classadoo autocommit

--- firebase-config.js
+++ firebase-config.js
@@ -10,7 +10,7 @@ var store = firebase.database().ref("challenge")
 
 function addMessage() {
   console.log("Adding message");
-  store.child("messages").push({message: "hail hydra!"})	
+  store.child("messages").push({message: "what is love? Baby dont hurt me dont hurt me no more."})	
 }
 
 function readMessages(callback) {
